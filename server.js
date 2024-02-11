31e091ca89b6915aad36044f19328e32ea122ec0
Ammar Zulfiqar <ammarzulfiqar@Ammars-MacBook-Air.local>
2024-02-12 00:13:15 +0500

    bruh

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ dotenv.config();
 import router from "./routes/products.js";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 import axios from "axios";
 const app =  express();
-
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json()) 
 app.use(router);
